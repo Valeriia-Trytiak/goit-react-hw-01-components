0706024ced148732cd7952df120c793a638f56df
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-09-29 00:09:28 +0200

    update table

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
 import user from "data/user.json";
 import data from "data/data.json";
 import friends from "data/friends.json";
+import transactions from "data/transactions.json";
 
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
-
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory"
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         gap: 15,
         flexDirection: 'column',
@@ -33,6 +34,8 @@ export const App = () => {
 {/* <Statistics stats={data} /> */}
 
 <FriendList friends={friends} />
+
+<TransactionHistory items={transactions} />
     </div>
   );
 };
